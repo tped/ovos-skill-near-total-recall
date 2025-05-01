0a21d61ddd241d74a2dcf9b2829c33091cd06f24
tped <tom@tpedersen.net>
2025-05-01 05:03:22 -0500

    Added some displays to see if we go through initialization
    Also back to default init def and super call

--- src/__init__.py
+++ src/__init__.py
@@ -24,8 +24,8 @@ DEFAULT_SETTINGS = {
 
 
 class NearTotalRecall(OVOSSkill):
-    # def __init__(self, *args, bus=None, **kwargs):
-    def __init__(self, bus=None, skill_id=None, *args, **kwargs):
+    def __init__(self, *args, bus=None, **kwargs):
+        # def __init__(self, bus=None, skill_id=None, *args, **kwargs):
         """The __init__ method is called when the Skill is first constructed.
         Note that self.bus, self.skill_id, self.settings, and
         other base class settings are only available after the call to super().
@@ -33,8 +33,8 @@ class NearTotalRecall(OVOSSkill):
         This is a good place to load and pre-process any data needed by your
         Skill, ideally after the super() call.
         """
-        # super().__init__(*args, bus=bus, **kwargs)
-        super().__init__(bus=bus, skill_id=skill_id, *args, **kwargs)
+        super().__init__(*args, bus=bus, **kwargs)
+        # super().__init__(bus=bus, skill_id=skill_id, *args, **kwargs)
 
         self.learning = True
         self.is_reciting = False  # Track if MeePi is currently babbling
@@ -91,6 +91,8 @@ class NearTotalRecall(OVOSSkill):
     def initialize(self):
         # merge default settings
         # self.settings is a jsondb, which extends the dict class and adds helpers like merge
+        self.log.info("Initializing Near-Total-Recall Skill")
+        self.log.info(f"Skill ID: {self.skill_id}")
         self.settings.merge(DEFAULT_SETTINGS, new_only=True)
 
     @classproperty
