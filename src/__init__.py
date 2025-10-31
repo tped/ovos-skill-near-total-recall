fb9da5bb85f8df77a2568a25c22d6703937e2bd9
tped <tom@tpedersen.net>
2025-10-31 05:10:28 -0500

    - Spoken Version @ startup

--- src/__init__.py
+++ src/__init__.py
@@ -111,7 +111,7 @@ class NearTotalRecall(OVOSSkill):
             self.speak_dialog("error_initialization")
 
         self.log.info(f"MeePi Databank Initialization Complete")
-        self.speak("MeePi's Near Total Recall is Alive - with New init Announcement")
+        self.speak("MeePi Near Total Recall is Alive.  Version 0 dot 2.  Stable")
 
     @classproperty
     def runtime_requirements(self):
